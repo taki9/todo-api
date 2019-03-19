0a173015e3d099ac141b1e068ca529b5b64732af
Péter Hauszknecht <peter.hauszknecht@guidesmiths.com>
2019-03-19 14:39:23 +0100

    improve error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,10 +1,8 @@
 module.exports = (err, req, res, next) => {
-  const [
-    message,
-    status = '500',
-    code = 'SOMETHING_WENT_WRONG'
-  ] = err.message.split('|');
+  const [message, status = '500', code = 'SOMETHING_WENT_WRONG'] = err.message.split('|');
 
-  res.status(status).send({ message, status, code });
+  res
+    .status(status)
+    .send({ message: status === '500' ? 'Something went wrong' : message, status: Number(status), code });
   next();
 };
